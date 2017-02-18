6e789758b5a6efea252c550ef7a25044d46cd62b
Juan Aparicio <apariciojuan30@gmail.com>
2017-02-17 23:28:41 -0300

    Agregue la clase Buses
    Agregue algunas de las funciones, pero tengo que implementar funciones
    de la clase Excursiones para las funciones:
    hayBusLibre(Date, Date): bool
    busLibre(Date, Date): String
    findCodigoExcursion(int): Bus

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -1,10 +1,24 @@
 package logica;
 
+import java.util.Date;
+
+import logica.Buses;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Bus busAux = new Bus("matricula", "marca", 4);
 		System.out.println("hola");
+		Buses bs = new Buses();
+		System.out.println("Antes de insertar.");
+		bs.imprimir();
+		bs.insert(busAux);
+		System.out.println("Despues de insertar");
+		bs.imprimir();
+		Date uno = new Date();
+		Date dos = new Date();
+		boolean aux = bs.hayBusLibre(uno, dos);
 	}
 
 }
