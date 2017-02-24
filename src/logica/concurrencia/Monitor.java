497fa450887079a87e8914380b6bb343f8b121e2
Juan Aparicio <apariciojuan30@gmail.com>
2017-02-24 17:27:04 -0300

    puse un comentario en el monitor

--- src/logica/concurrencia/Monitor.java
+++ src/logica/concurrencia/Monitor.java
@@ -1,27 +1,50 @@
 package logica.concurrencia;
 
 public class Monitor {
+	/*TODO revisar monitor*/
 	int cantLectores;
+	int cantEscritores;
 	boolean leyendo;
 	
+	/*Acceso a lectura: si nadie esta escribiendo o quiere escribir, leo*/
+	/*Acceso a escritura: si nadie esta leyendo o escribiendo, escribo*/
 	public Monitor(){
 		this.cantLectores = 0;
-		leyendo = false;
+		this.cantEscritores = 0;
 	}
 	
 	public synchronized void startRead(){
-		
+		while(cantEscritores > 0){
+			try {
+				this.wait(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		/*Si llego aca es porque no hay gente escribiendo*/
+		cantLectores++;
 	}
 	
 	public synchronized void endRead(){
-		
+		cantLectores--;
+		this.notify(); /*Esto va aca???*/
 	}
 	
 	public synchronized void startWrite(){
-		
+			try {
+				this.wait(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println(e.toString());
+			}
+		}
+		cantEscritores++;
 	}
 	
 	public synchronized void endWrite(){
-		
+		cantEscritores--;
+		this.notify();
 	}
 }
