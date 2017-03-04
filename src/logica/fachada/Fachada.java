2ea9adfeb7d3493dc85d5eeade9a8207de4a747b
guillegandolfo <guillegandolfo@hotmail.com>
2017-03-04 16:51:34 -0300

    Servidor

--- src/logica/fachada/Fachada.java
+++ src/logica/fachada/Fachada.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import java.util.Properties;
-
 import logica.Excepciones.colecciones.*;
 import logica.Excepciones.objetos.*;
 import logica.objetos.*;
