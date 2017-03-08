29641f36b9810e3b93b7c261a71bde77366bfe7b
Juan Aparicio <apariciojuan30@gmail.com>
2017-03-08 16:35:18 -0300

    Terminado iterador personalizado

--- src/logica/colecciones/Iterador.java
+++ src/logica/colecciones/Iterador.java
@@ -1,5 +1,34 @@
 package logica.colecciones;
 
-public class Iterador {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Iterador<T> implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	int max;
+	int actual;
+	List<T> contenido;
+	
+	public Iterador(){
+		contenido = new ArrayList<T>();
+		max = 0;
+		actual = 0;
+	}
+	
+	public void add(T insertar){
+		contenido.add(insertar);
+		max++;
+	}
+	
+	public boolean hasNext(){
+		return(actual < max);
+	}
+	
+	public T next(){
+		actual++;
+		return contenido.get(actual - 1);
+	}
+	
 }
