69bc276d1ea73ca3f7e9bbe9385b52c04ff6f091
Juan Aparicio <apariciojuan30@gmail.com>
2017-03-20 17:28:51 -0300

    cambio por el constructor de VOBoleto

--- src/servidor/Cliente.java
+++ src/servidor/Cliente.java
@@ -85,7 +85,7 @@ public class Cliente {
 			fachada.registroNuevoBus(new VOBus("aaa001", "asd", 4));
 			fachada.registroNuevoBus(new VOBus("aaa002", "asddasd", 5));
 			fachada.registroNuevaExcursion(new VOExcursion("ch001", "monte", new Date(2017, 10, 10, 10, 10), new Date(2017, 10, 10, 10, 20), 200.0));
-			fachada.ventaBoleto(new VOBoleto("ch001", "monte", 15, 91281074, "Comun", 0.0));
+			fachada.ventaBoleto(new VOBoleto("ch001", "monte", 15, 91281074, 0.0));
 			fachada.reasignacionExcursion("ch001");
 			Iterador<VOBoleto2> ite = fachada.listadoBoletosExcursion("ch001", "Comun");
 			
